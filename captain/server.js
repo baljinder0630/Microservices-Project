23c80da0e8e90d1096ae5dd73e7c27a3092422c3
baljinder0630 <baljindersingh41265@gmail.com>
2025-07-13 15:49:17 +0530

    Added Captain Service

--- captain/server.js
+++ captain/server.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const app = require('./app');
 
+
 const server = http.createServer(app);
 
-server.listen(3001, () => {
-    console.log('User service is running on port 3001');
+
+
+
+server.listen(3002, () => {
+    console.log('captain service is running on port 3002');
 });
