215223b2bb025cf8350e2c11bb470ef6d47a6de8
baljinder0630 <baljindersingh41265@gmail.com>
2025-07-21 22:13:37 +0530

    Added whitespace for improved readability in user server file

--- ride/server.js
+++ ride/server.js
@@ -1,12 +1,9 @@
 const http = require('http');
 const app = require('./app');
 
-
 const server = http.createServer(app);
 
 
-
-
-server.listen(3001, () => {
-    console.log('User service is running on port 3001');
-});
+server.listen(3003, () => {
+    console.log('ride service is running on port 3003');
+})
